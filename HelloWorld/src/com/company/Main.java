3ca6164320b8a34f5cdad927a193e823b13c8424
Vadim Makarenko <mcar17@yandex.ru>
2020-09-05 16:47:16 +0300

    Starting commit HelloWorld

---
+++ HelloWorld/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean flag = false;
+        if (flag){
+            System.out.println("Hello World!");
+        }
+        else
+        {
+            byte a =100;
+            byte b = 100;
+            byte c = (byte)(a+b);
+            System.out.println(c);
+        }
+
+    }
+}
+
